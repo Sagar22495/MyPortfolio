e5263781f9bcc6c2354e3da765738f5eaecc18b5
unknown <jagtaps100@gmail.com>
2025-10-21 21:41:40 +0530

    added beyondwork page

--- script.js
+++ script.js
@@ -15,4 +15,15 @@ document.querySelectorAll('#mainNav a').forEach(link => {
   });
 });
 
+//BEYOND WORK SLIDE//
 
+// ======= NAVBAR BURGER TOGGLE =======
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+if (burger && navLinks) {
+  burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+  });
+}
