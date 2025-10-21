31dc385ad37fca2772d03231a0f09ad84f270bbe
unknown <jagtaps100@gmail.com>
2025-10-21 22:06:34 +0530

    removed beyondwork code

--- script.js
+++ script.js
@@ -16,14 +16,3 @@ document.querySelectorAll('#mainNav a').forEach(link => {
 });
 
 //BEYOND WORK SLIDE//
-
-// ======= NAVBAR BURGER TOGGLE =======
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-if (burger && navLinks) {
-  burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    burger.classList.toggle('toggle');
-  });
-}
